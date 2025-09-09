d447f8df913a960fd0657616feffdc96a46ac81d
yashgoyal9898 <yashgoyal9898@gmail.com>
2025-09-09 12:03:49 +0530

    access token file change

--- access_token.py
+++ access_token.py
@@ -1,14 +1,12 @@
 import requests
 import json
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
-# --- 🔐 Credentials (Replace these with environment variables if needed) ---
-APP_ID_HASH = "ac10a60166f494b5c09cbaba5f07fa34f3ff65ef26aa0583f99078b4d186a6c5"
-REFRESH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsiZDoxIiwiZDoyIiwieDowIiwieDoxIiwieDoyIl0sImF0X2hhc2giOiJnQUFBQUFCb3Y1LTR0MEhRczJDWXB3ZTRNbjhudHFjVENTRHE0OC1SY1pTSllqSUhqU21LT2J1YmNDa1o4Vi1tNUt1UnZ6bjVCbmlCYktITlZZOGdETlRnLUlHTEY1V0RSMWJ2MlJKaFB5ZTJyUkt3UnBiNVJERT0iLCJkaXNwbGF5X25hbWUiOiIiLCJvbXMiOiJLMSIsImhzbV9rZXkiOiI1NDQ5YzNkMDA2MjVjYTUwM2JhMWRhNGIyNTZiNzU3ZmQzNjhiYmY4Y2VlNzA5MjFmOTY2NDM5NyIsImlzRGRwaUVuYWJsZWQiOiJZIiwiaXNNdGZFbmFibGVkIjoiTiIsImZ5X2lkIjoiRkFCMDYzNzgiLCJhcHBUeXBlIjoxMDAsImV4cCI6MTc1ODY3MzgwMCwiaWF0IjoxNzU3Mzg4NzI4LCJpc3MiOiJhcGkuZnllcnMuaW4iLCJuYmYiOjE3NTczODg3MjgsInN1YiI6InJlZnJlc2hfdG9rZW4ifQ.3PWYmDrOrcQE-MJT5TNhOcapyamSGayM3ZmhiYBWohQ"
-PIN = "1999"  # Consider moving to env var or input()
+
+PIN = os.getenv("PIN")  
 
 # --- Endpoint ---
 URL = "https://api-t1.fyers.in/api/v3/validate-refresh-token"
@@ -16,8 +14,8 @@ URL = "https://api-t1.fyers.in/api/v3/validate-refresh-token"
 # --- 📦 Payload ---
 payload = {
     "grant_type": "refresh_token",
-    "appIdHash": APP_ID_HASH,
-    "refresh_token": REFRESH_TOKEN,
+    "appIdHash": os.getenv("APP_ID_HASH"),
+    "refresh_token": os.getenv("FYERS_REFRESH_TOKEN"),
     "pin": PIN
 }
 
